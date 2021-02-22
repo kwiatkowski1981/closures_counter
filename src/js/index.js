f585e394dbaec8d56afb592d091d0d9db57e58c5
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-22 10:22:42 +0100

    example of closure.

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,26 @@ import '../scss/main.scss';
 console.log('HELLO 🚀')
 
 
+
+
+
+let number = 0;
+
+const add = () => {
+    number++;
+    document.body.textContent = `Aktualny stan licznika to ${number}`;
+}
+document.addEventListener('click', add);
+
+function x() {
+    let digit = 0;
+    function y() {
+        console.log(`%c Wyświetlam: ${digit}`, "color: red;");
+    }
+    return y;
+}
+const example = x();
+example();
+
+
+
